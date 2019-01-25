428f94fab6eb325b2c725797545a71beedca31e7
justin <justin.barry.e@gmail.com>
2019-01-24 20:07:37 -0500

    fixed findnode_prev for now

--- datastructures/linkedlist.py
+++ datastructures/linkedlist.py
@@ -12,21 +12,25 @@ class LinkedList():
         last = self.traverse(lambda *args: None)
         ins = self.Node(val)
         last.next = ins
+        self.size += 1
 
     def insert(self, val, i):
-        if self.size == 0:
+        print("inserting {} into {}".format(val, i))
+        if not self.head or self.size == 0:
             self.head = self.Node(val)
         else:
-            cur = self.traverse()
+            cur = self.findnode_prev(i)
             ins = self.Node(val, cur.next)
             cur.next = ins
+        self.size += 1
 
-    def findpos(self, i):
+    def findnode_prev(self, i):
         cur = self.head
-        pos = 0
-        while cur.next:
-            if pos == i:
-                return cur
+        if i == 0:
+            return cur
+        pos = 1
+        while cur.next and pos != i:
+            pos += 1
             cur = cur.next
         return cur
 
@@ -50,9 +54,10 @@ class LinkedList():
 
 def main():
     L = LinkedList()
-    print("list size: {}".format(L.size))
     L.insert(5, 0)
     L.push(6)
+    L.push(7)
+    L.insert(3, 1)
     L.printlist()
 
 if __name__ == "__main__":
