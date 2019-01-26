bee5fc5deec349f0502eef7e59d7cfc3d3d4b6e9
justin <justin.barry.e@gmail.com>
2019-01-26 11:12:31 -0500

    added file reading to tree. Added getters/setters to Node

--- datastructures/Trie.java
+++ datastructures/Trie.java
@@ -1,13 +1,43 @@
 import java.util.*;
+import java.util.function.*;
 import java.lang.*;
 import java.io.*;
 
 public class Trie {
 
+    // TODO: Make generic
     private class Node {
-	private HashMap<String, Node> children;
-	private ArrayList<Integer> postitions;
+
+	private Map<String, Node> children;
+	private List<Integer> positions;
 	private String value;
+
+	public Node() {
+	}
+
+	public void setChildren(Map<String, Node> children) {
+	    this.children = children;
+	}
+
+	public Map<String, Node> getChildren() {
+	    return children;
+	}
+	
+	public void setPositions(List<Integer> positions) {
+	    this.positions = positions;
+	}
+
+	public List<Integer> getPositions() {
+	    return positions;
+	}
+
+	public void setValue(String value) {
+	    this.value = value;
+	}
+
+	public String getValue() {
+	    return value;
+	}
     }
 
     private Node root;
@@ -18,10 +48,41 @@ public class Trie {
 
     public Trie(List<String> words) {
 	root = new Node();
-	// TODO: for word : words - insert into trie
+	for (String word : words)
+	    insert(word);
+    }
+
+    private Node traverse(Node o) {
+
+	return null;
+    }
+
+    private void insert(String s) {
+	if (root == null)
+	    throw new IllegalArgumentException("root is null");
+
     }
 
     public static void main(String[] args) {
-	System.out.println("hello");
+	File f = new File("/home/justin/git/DataStructuresPy" +
+		"/datastructures/trietext.txt");
+	Scanner sc;
+	try {
+	    sc = new Scanner(f);
+	    List<String> words = new ArrayList<String>();
+	    while (sc.hasNext()) {
+		String word = sc.next();
+		words.add(word);
+	    }
+	    Trie t = new Trie(words);
+	    int i = 0;
+	    for (String word : words) {
+		t.insert(word);
+		if (i == 0)
+		    break;
+	    }
+	} catch (FileNotFoundException e) {
+	    System.out.println(e.getMessage());
+	}
     }
 }
