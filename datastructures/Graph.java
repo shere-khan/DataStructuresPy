66cdd913f62393c1bd80a6856f8fdf3765fff765
justin <justin.barry.e@gmail.com>
2019-01-26 20:12:40 -0500

    added Graph::addEdge and Graph::addNodeToTable. Both untested

--- datastructures/Graph.java
+++ datastructures/Graph.java
@@ -1,8 +1,8 @@
-java.util.*;
-java.lang.*;
-java.util.function.*;
+import java.util.*;
+import java.lang.*;
+import java.util.function.*;
 
-public Graph {
+public class Graph {
     private class Node {
 
 	public String value;
@@ -24,5 +24,24 @@ public Graph {
 	}
     }
 
+    private Map<Node, Map<Node, Edge>> table;
+    private boolean isDirected;
 
+    public Graph(boolean isDirected) {
+	this.isDirected = isDirected;
+	table = new HashMap<Node, Map<Node, Edge>>();
+    }
+    
+    private void addNodeToTable(Node n) {
+	for (Map<Node, Edge> map: table.values())
+	    map.put(n, null);
+    }
+
+    private void addEdge(Node n1, Node n2, Edge e) {
+	table.get(n1).put(n2, e);
+    }
+
+    public static void main(String[] args) {
+	System.out.println("hello");
+    }
 }
