ca8d04f553362cc4b8f24d055cf393caece8c784
justin <justin.barry.e@gmail.com>
2019-01-26 19:54:41 -0500

    made private classes Node and Edge

--- datastructures/Graph.java
+++ datastructures/Graph.java
@@ -4,11 +4,25 @@ java.util.function.*;
 
 public Graph {
     private class Node {
-	public Map<String, Node> children;
-	public List<Integer> positions;
+
 	public String value;
 
-	public Node() {
+	public Node(String value) {
+	    this.value = value;
 	}
     }
+
+    private class Edge {
+
+	public Integer weight;
+	public Node from;
+	public Node to;
+
+	public Edge(Node from, Node to) {
+	    this.from = from;
+	    this.to = to;
+	}
+    }
+
+
 }
