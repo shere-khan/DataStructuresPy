8f754abf44b012a8673254379134320ccebc5283
justin <justin.barry.e@gmail.com>
2019-01-26 19:50:26 -0500

    making fields public because I'm lazy

--- datastructures/Graph.java
+++ datastructures/Graph.java
@@ -4,35 +4,11 @@ java.util.function.*;
 
 public Graph {
     private class Node {
-	private Map<String, Node> children;
-	private List<Integer> positions;
-	private String value;
+	public Map<String, Node> children;
+	public List<Integer> positions;
+	public String value;
 
 	public Node() {
-	}
-
-	public void setChildren(Map<String, Node> children) {
-	    this.children = children;
-	}
-
-	public Map<String, Node> getChildren() {
-	    return children;
-	}
-	
-	public void setPositions(List<Integer> positions) {
-	    this.positions = positions;
-	}
-
-	public List<Integer> getPositions() {
-	    return positions;
-	}
-
-	public void setValue(String value) {
-	    this.value = value;
-	}
-
-	public String getValue() {
-	    return value;
 	}
     }
 }
